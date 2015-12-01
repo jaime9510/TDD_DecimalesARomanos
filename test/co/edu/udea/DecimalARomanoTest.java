c7a33ef70f538e0e736f5171a7cd3e93a05399e1
Jaime Londoño <jaime.londono2@udea.edu.co>
2015-11-30 20:18:55 -0500

    Test convertir número 1 a romano

--- test/co/edu/udea/DecimalARomanoTest.java
+++ test/co/edu/udea/DecimalARomanoTest.java
@@ -1,5 +1,24 @@
 package co.edu.udea;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class DecimalARomanoTest {
 
+	@Test
+	public void testConvertirNum1() {
+		
+		//Arrange
+		int decimal = 1;
+		String romano = "";
+		DecimalARomano decimal_romano = null;
+		
+		//Act
+		decimal_romano = new DecimalARomano();
+		romano = decimal_romano.convertir(decimal);
+		
+		//Assert
+		assertTrue("Se convierte número 1 a romano", romano.equals("I"));
+	}
 }
